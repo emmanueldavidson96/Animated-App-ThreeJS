4ac47f4c01362a39a4434fb0d7c1251a9445475b
Emmanuel <emmanueldavidson524@gmail.com>
2025-08-04 11:46:24 +0100

    Completed Animated Site with ThreeJS

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
+import Experiences from './sections/Experiences'
+import Testimonial from './sections/Testimonial'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experiences />
+      <Testimonial />
+      <Contact />
+      <Footer />     
+    </div>
+  )
+}
+
+export default App
